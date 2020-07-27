7530650f502003a0c179c0a524de7939d2a5c3d6
Atsushi Nagase <a@ngs.io>
2020-07-27 21:26:39 +0900

    More tests

--- Tests/JMATidesTests/JMATidesTests.swift
+++ Tests/JMATidesTests/JMATidesTests.swift
@@ -6,9 +6,15 @@ final class JMATidesTests: XCTestCase {
         let url = URL(fileURLWithPath: #file).deletingLastPathComponent().deletingLastPathComponent().appendingPathComponent("Fixtures/TK.txt")
         let data = try! Data(contentsOf: url)
         let parser = Parser(data: data)
-        parser.records.forEach { record in
-            XCTAssertEqual(record.locationId, "TK")
-            XCTAssertEqual(record.levels.count, 24)
+        XCTAssertEqual(parser.records.count, 366)
+        for d in 0..<366 {
+            XCTAssertEqual(parser.records[d].locationId, "TK")
+            XCTAssertEqual(parser.records[d].levels.count, 24)
+            let date = Date(timeIntervalSince1970: TimeInterval(1577804400 + d * 24 * 60 * 60))
+            XCTAssertEqual(parser.records[d].date, date)
+            for h in 0..<24 {
+                XCTAssertEqual(parser.records[d].levels[h].date, date.addingTimeInterval(TimeInterval(h * 60 * 60)))
+            }
         }
     }
 
